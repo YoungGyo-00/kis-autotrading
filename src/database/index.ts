d98d9fffb96a8228bd39f9ab51f3ec077032ccb7
younggyo <lee4519698@hanmail.net>
2022-12-28 03:35:45 +0900

    📝 Feat(database/index.ts) : Entity 자동 생성 코드 추가

--- src/database/index.ts
+++ src/database/index.ts
@@ -15,7 +15,7 @@ const configs: Signature = {
         synchronize: true,
         logging: true,
         dropSchema: false,
-        entities: [],
+        entities: ["./**/*{.ts}"],
     },
 };
 
