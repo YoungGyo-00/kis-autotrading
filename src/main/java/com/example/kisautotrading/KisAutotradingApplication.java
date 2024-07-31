2b04a38cb1e48cf3c23aa73531422b927a2771eb
younggyo <lee4519698@hanmail.net>
2024-08-01 08:32:40 +0900

    # <[type]>: <subject> ##### Subject 50 characters ################# -> chore : java 환경 설정
    # Body Message
    ######## Body 72 characters ####################################### ->
    
    # Issue Tracker Number or URL (optional) ->
    
    # --- COMMIT END ---
    # Type can be
    #   feat    : 새로운 기능 추가
    #   fix     : 버그/오타(typo)/로직 등 코드를 수정한 경우
    #   refactor: 코드 리팩토링
    #   style   : 코드 포맷팅, 세미콜론 누락 수정 등 내부 로직 변경이 없이 코드를 수정한 경우
    #   docs    : README 문서 수정
    #   test    : 테스트 코드, 리팩토링 테스트 코드 추가
    #   chore   : 빌드 업무 수정, 패키지 매니저 수정
    #   remove  : 코드/파일 삭제
    #   dep     : 패키지 설치/삭제 등 의존성 관련 수정
    #   etc     : 기타
    # ------------------
    # Remember me ~
    #   Capitalize the subject line
    #     제목줄은 대문자로 시작한다.
    #   Use the imperative mood in the subject line
    #     제목줄은 명령어로 작성한다.
    #   Do not end the subject line with a period
    #     제목줄은 마침표로 끝내지 않는다.
    #   Separate subject from body with a blank line
    #     본문과 제목에는 빈줄을 넣어서 구분한다.
    #   Use the body to explain what and why vs. how
    #     본문에는 "어떻게" 보다는 "왜"와 "무엇을" 설명한다.
    #   Can use multiple lines with "-" for bullet points in body
    #     본문에 목록을 나타낼때는 "-"로 시작한다.
    # ------------------

---
+++ src/main/java/com/example/kisautotrading/KisAutotradingApplication.java
@@ -0,0 +1,13 @@
+package com.example.kisautotrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KisAutotradingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KisAutotradingApplication.class, args);
+    }
+
+}
