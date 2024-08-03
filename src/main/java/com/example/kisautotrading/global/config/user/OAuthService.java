c5ebb96a5d921de2bf5a88270519520e79446617
younggyo <lee4519698@hanmail.net>
2024-08-03 20:59:02 +0900

    update : 토큰 시간 만료 검증

--- src/main/java/com/example/kisautotrading/global/config/user/OAuthService.java
+++ src/main/java/com/example/kisautotrading/global/config/user/OAuthService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.time.Instant;
 import java.util.Map;
 
 @Slf4j
@@ -23,20 +24,29 @@ public class OAuthService {
     private String APP_SECRET;
 
     private final WebClient webClient;
+    private String accessToken;
+    private Instant tokenExpiry;
 
     public String getToken() {
-        OAuthReqeustDto requestBody = OAuthReqeustDto.builder()
-                .grant_type("client_credentials")
-                .appkey(APP_KEY)
-                .appsecret(APP_SECRET)
-                .build();
+        if (accessToken == null || Instant.now().isAfter(tokenExpiry)) {
+            OAuthReqeustDto requestBody = OAuthReqeustDto.builder()
+                    .grant_type("client_credentials")
+                    .appkey(APP_KEY)
+                    .appsecret(APP_SECRET)
+                    .build();
 
-        return webClient.post()
+            Map<String, Object> response = webClient.post()
                 .uri("/oauth2/tokenP")
                 .bodyValue(requestBody)
                 .retrieve()
                 .bodyToMono(Map.class)
-                .map(response -> response.get("access_token").toString())
                 .block();
+
+            accessToken = response.get("access_token").toString();
+            long expiresIn = Long.parseLong(response.get("expires_in").toString());
+            tokenExpiry = Instant.now().plusSeconds(expiresIn);
+        }
+
+        return accessToken;
     }
 }
