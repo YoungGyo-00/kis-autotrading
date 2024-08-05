2d6e1db2111bc933d7638921629d31368a72434b
younggyo <lee4519698@hanmail.net>
2024-08-05 13:57:46 +0900

    fix : 주문 엔티티 수정

--- src/main/java/com/example/kisautotrading/domain/trading/domain/OrderCancel.java
+++ src/main/java/com/example/kisautotrading/domain/trading/domain/OrderCancel.java
@@ -1,4 +1,4 @@
-package com.example.kisautotrading.domain.order.domain;
+package com.example.kisautotrading.domain.trading.domain;
 
 import com.example.kisautotrading.global.common.entity.BaseTimeEntity;
 import jakarta.persistence.*;
@@ -10,22 +10,22 @@ import lombok.NoArgsConstructor;
 // 주문취소내역
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Entity(name = "ORD_CNCL_DLST")
+@Entity(name = "ORD_CNCL_LST")
 public class OrderCancel extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "order_list_id", nullable = false)
-    private OrderList orderList;
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
 
     @Column(name = "CNCL_SECOMP_ORD_NO")
     private String cancelSecompOrderNo; // 취소증권사주문번호
 
     @Builder
-    public OrderCancel(OrderList orderList, String cancelSecompOrderNo) {
-        this.orderList = orderList;
+    public OrderCancel(Order order, String cancelSecompOrderNo) {
+        this.order = order;
         this.cancelSecompOrderNo = cancelSecompOrderNo;
     }
 }
